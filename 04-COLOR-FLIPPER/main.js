b77d24d599364df423b6fa42e0db1dd08ee1ae36
PaulAjii <paulajii134@gmail.com>
2022-10-17 16:38:35 +0100

    JS functionality

--- 04-COLOR-FLIPPER/main.js
+++ 04-COLOR-FLIPPER/main.js
@@ -1,1 +1,25 @@
-console.log("Color Flipper");
+const btn = document.querySelector(".btn")
+const colorValue = document.querySelector(".color-value")
+const container = document.querySelector(".container")
+
+const colors = ["green", "blue", "red", "#f3f4d2", "aliceblue", "whitesmoke"]
+// Other Colors can be added but adjustments need to bee added to the code
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
+
+btn.addEventListener("click", () => {
+    const randomNumber = getRandomNumber()
+
+    colorValue.textContent = colors[randomNumber]
+    container.style.backgroundColor = colors[randomNumber]
+
+    if(randomNumber === 0 || randomNumber === 1 || randomNumber === 2) {
+        container.style.color = "#fff"
+        btn.style.color = "#fff"
+    } else {
+        container.style.color = "#555"
+        btn.style.color = "#555"
+    }
+})
